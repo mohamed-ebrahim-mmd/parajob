fa6ac7de21356d6a62f739f15e1c1fae7562404a
*mohamedebrahim7* <mohamed7ebrahim7@gmail.com>
2025-10-14 11:39:19 +0300

    fix git issue

--- lib/features/onboarding/widgets/onboarding_view1.dart
+++ lib/features/onboarding/widgets/onboarding_view1.dart
@@ -3,9 +3,10 @@
  ==================================================================
 */
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:para_job/packages/route_manager/controller/routing_controller.dart';
+import 'package:para_job/packages/themeing/app_colors.dart';
+import 'package:para_job/packages/themeing/media_query_values.dart';
 import 'package:para_job/res/app_asset_paths.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingView1 extends StatelessWidget {
   const OnboardingView1({super.key});
@@ -22,13 +23,73 @@ class OnboardingView1 extends StatelessWidget {
         ),
 
         // Text at the bottom
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: TextButton(
-            onPressed: () {
-              Get.find<RoutingController>().goAuthChoiceScreen();
-            },
-            child: Text('text'),
+        Positioned(
+          bottom: context.hPct(10),
+          left: 0,
+          right: 0,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: context.wPct(8)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // --- Onboarding Text ---
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    text: 'Browe to find the',
+                    style: TextStyle(
+                      color: AppColors.pureWhite,
+                      fontSize: context.wPct(4.5),
+                      fontWeight: FontWeight.w600,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: ' perfect job ',
+                        style: TextStyle(color: AppColors.aquaTeal),
+                      ),
+                      TextSpan(text: 'for you'),
+                    ],
+                  ),
+                ),
+
+                context.hBox(4),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AnimatedSmoothIndicator(
+                      activeIndex: 0,
+                      count: 3,
+                      effect: ExpandingDotsEffect(
+                        dotHeight: 8,
+                        dotWidth: 8,
+                        activeDotColor: AppColors.aquaTeal,
+                        dotColor: Colors.white,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        // Handle skip action
+                      },
+                      child: Row(
+                        children: [
+                          Text(
+                            "skip",
+                            style: TextStyle(fontSize: context.wPct(4)),
+                          ),
+                          context.wBox(1),
+                          Icon(
+                            Icons.double_arrow_rounded,
+                            size: context.wPct(4),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ],
--- lib/features/onboarding/widgets/onboarding_view2.dart
+++ lib/features/onboarding/widgets/onboarding_view2.dart
@@ -3,7 +3,10 @@
  ==================================================================
 */
 import 'package:flutter/material.dart';
+import 'package:para_job/packages/themeing/app_colors.dart';
+import 'package:para_job/packages/themeing/media_query_values.dart';
 import 'package:para_job/res/app_asset_paths.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingView2 extends StatelessWidget {
   const OnboardingView2({super.key});
@@ -17,18 +20,72 @@ class OnboardingView2 extends StatelessWidget {
           AppAssetPaths.onboardingScreenBackground2,
           fit: BoxFit.cover,
         ),
-        Align(
-          alignment: Alignment.bottomCenter,
+        Positioned(
+          bottom: context.hPct(10),
+          left: 0,
+          right: 0,
           child: Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Text(
-              'Discover great features!',
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 22,
-                fontWeight: FontWeight.w600,
-              ),
+            padding: EdgeInsets.symmetric(horizontal: context.wPct(8)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // --- Onboarding Text ---
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    text: 'Finad all the ',
+                    style: TextStyle(
+                      color: AppColors.pureWhite,
+                      fontSize: context.wPct(4.5),
+                      fontWeight: FontWeight.w600,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'job details ',
+                        style: TextStyle(color: AppColors.aquaTeal),
+                      ),
+                      TextSpan(text: 'you might need.'),
+                    ],
+                  ),
+                ),
+
+                context.hBox(4),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AnimatedSmoothIndicator(
+                      activeIndex: 1,
+                      count: 3,
+                      effect: ExpandingDotsEffect(
+                        dotHeight: 8,
+                        dotWidth: 8,
+                        activeDotColor: AppColors.aquaTeal,
+                        dotColor: Colors.white,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        // Handle skip action
+                      },
+                      child: Row(
+                        children: [
+                          Text(
+                            "skip",
+                            style: TextStyle(fontSize: context.wPct(4)),
+                          ),
+                          context.wBox(1),
+                          Icon(
+                            Icons.double_arrow_rounded,
+                            size: context.wPct(4),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
