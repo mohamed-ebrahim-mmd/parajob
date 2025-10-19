14e478e5e6c45b465625274cb7242148ebd7be9a
marymark-919 <mary.mark@moselaymd.com>
2025-10-19 16:31:56 +0300

    Refactor CreateAccountSkillsScreen and add SkillItem widget

--- lib/features/registration/create_account_skills/create_account_skills_screen.dart
+++ lib/features/registration/create_account_skills/create_account_skills_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
+import 'package:para_job/features/registration/widgets/skill_item.dart';
 import 'package:para_job/features/registration/widgets/stepper.dart';
 import 'package:para_job/packages/themeing/media_query_values.dart';
 
@@ -13,7 +13,7 @@ class CreateAccountSkillsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-         appBar: AppBar(
+      appBar: AppBar(
         surfaceTintColor: Colors.transparent,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_ios_new),
@@ -23,8 +23,6 @@ class CreateAccountSkillsScreen extends StatelessWidget {
         ),
       ),
 
-
-    
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: context.wPct(5)),
         child: SingleChildScrollView(
@@ -52,19 +50,19 @@ class CreateAccountSkillsScreen extends StatelessWidget {
               ),
               context.hBox(3),
 
-
-       Row(
-  children: [
-    Flexible(
-      child: Wrap(
-        spacing: context.wPct(3),
-        runSpacing: context.wPct(3),
-        children: skills.map((skill) => SkillItem(skill: skill)).toList(),
-      ),
-    ),
-  ],
-),
-            
+              Row(
+                children: [
+                  Flexible(
+                    child: Wrap(
+                      spacing: context.wPct(3),
+                      runSpacing: context.wPct(3),
+                      children: skills
+                          .map((skill) => SkillItem(skill: skill))
+                          .toList(),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
@@ -82,7 +80,7 @@ class CreateAccountSkillsScreen extends StatelessWidget {
             FilledButton(
               onPressed: () {
                 Get.toNamed(
-                  "${Routes.createAccount}${Routes.createAccountOTP}${Routes.createAccountSetPass}${Routes.createAccountFrontID}${Routes.createAccountBackID}${Routes.createAccountPicWithID}${Routes.educationInfo}${Routes.educationPic}${Routes.create_account_skills}${Routes.create_account_cv}"
+                  "${Routes.createAccount}${Routes.createAccountOTP}${Routes.createAccountSetPass}${Routes.createAccountFrontID}${Routes.createAccountBackID}${Routes.createAccountPicWithID}${Routes.educationInfo}${Routes.educationPic}${Routes.create_account_skills}${Routes.create_account_cv}",
                 );
               },
               child: Text("Continue"),
@@ -102,55 +100,8 @@ class CreateAccountSkillsScreen extends StatelessWidget {
           ],
         ),
       ),
-   
-   
     );
   }
 }
-List<String> skills = [
-  "Plumbing",
-  "Electrical Work",
-  "Carpentry",
-  "Painting",
 
-
-];
-
-
-class SkillItem
- extends StatelessWidget {
-  const SkillItem
-  ({super.key, required this.skill});
-  final String skill;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container( 
-      height: context.hPct(5),      
-      // width: context.wPct(3),
-
-      padding: EdgeInsets.symmetric(
-        horizontal: context.wPct(1),
-      //  vertical: context.hPct(1.5),
-      ),
-      decoration: BoxDecoration(
-        color: AppColors.aquaTeal8,
-        border: Border.all(color: AppColors.aquaTeal),
-        borderRadius: BorderRadius.circular(context.wPct(3),)
-      ),
-      child: Row( mainAxisSize: MainAxisSize.min,mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            skill,
-            style: TextStyle(
-              color: AppColors.aquaTeal,
-              fontSize: context.wPct(3.4),
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-          IconButton(onPressed: (){}, icon: Icon(Icons.close,color: AppColors.aquaTeal,size: context.wPct(4),))
-        ],
-      ),
-    );
-  }
-}
+List<String> skills = ["Plumbing", "Electrical Work", "Carpentry", "Painting"];
