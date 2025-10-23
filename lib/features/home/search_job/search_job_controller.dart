b833ad36992edbae90ffa5e6505cc6dfe625e977
*mohamedebrahim7* <mohamed7ebrahim7@gmail.com>
2025-10-23 18:22:06 +0300

    finish bottom sheet ui for search filter

--- lib/features/home/search_job/search_job_controller.dart
+++ lib/features/home/search_job/search_job_controller.dart
@@ -2,16 +2,31 @@
  Mohamed Ebrahim | mohamed7ebrahim7@gmail.com | 2025-10-23 11:49 AM
  ==================================================================
 */
+import 'package:flutter/material.dart' show DropdownMenuEntry;
 import 'package:get/get.dart';
 import 'package:para_job/packages/api_client/api_client.dart';
 
 class SearchJobController extends GetxController {
   // Unified call state
   final searchDataCallState = ApiCallState.loading.obs;
-
+  int? selectedSkillId;
+  int? selectedCompanyId;
+  String? selectedJobType;
+  String? selectedJobCategory;
   // Data
   List<Skill> skills = [];
   List<CompanyListItem> companies = [];
+
+  final jobTypeMenuEntries = [
+    DropdownMenuEntry<String>(value: 'part_time', label: 'Part time'),
+    DropdownMenuEntry<String>(value: 'full_time', label: 'Full time'),
+  ];
+
+  final jobCategoriesEntries = [
+    DropdownMenuEntry<String>(value: 'flexible', label: 'Flexible'),
+    DropdownMenuEntry<String>(value: 'non_flexible', label: 'Non Flexible'),
+    DropdownMenuEntry<String>(value: 'hot_job', label: 'Hot Job'),
+  ];
 
   @override
   void onInit() {
--- lib/features/home/search_job/search_job_screen.dart
+++ lib/features/home/search_job/search_job_screen.dart
@@ -3,11 +3,18 @@
  ==================================================================
 */
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:para_job/features/home/search_job/search_job_controller.dart';
+import 'package:para_job/features/home/search_job/widgets/show_filter_bottom_sheet.dart';
+import 'package:para_job/packages/api_client/src/enums/api_call_state_enum.dart'
+    show ApiCallState;
 import 'package:para_job/packages/themeing/app_colors.dart';
 import 'package:para_job/packages/themeing/media_query_values.dart';
+import 'package:para_job/packages/ui_components/error_screen.dart';
 
 class SearchJobScreen extends StatelessWidget {
-  const SearchJobScreen({super.key});
+  final controller = Get.put(SearchJobController());
+  SearchJobScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -23,26 +30,45 @@ class SearchJobScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: context.wPct(5)),
-        child: Column(
-          children: [
-            context.hBox(2),
-            TextField(
-              onTap: () {},
-              decoration: InputDecoration(
-                hintText: 'Search jobs, companies..',
-                prefixIcon: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.search),
+        child: Obx(() {
+          switch (controller.searchDataCallState.value) {
+            case ApiCallState.loading:
+              return const Center(child: CircularProgressIndicator());
+
+            case ApiCallState.success:
+              return Column(
+                children: [
+                  context.hBox(2),
+                  TextField(
+                    onTap: () {},
+                    decoration: InputDecoration(
+                      hintText: 'Search jobs, companies..',
+                      prefixIcon: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.search),
+                      ),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          showFilterBottomSheet();
+                        },
+                        icon: const Icon(Icons.tune),
+                      ),
+                      filled: false,
+                    ),
+                  ),
+                ],
+              );
+
+            case ApiCallState.failure:
+              return Center(
+                child: ErrorScreen(
+                  onPressed: () {
+                    controller.fetchSearchData();
+                  },
                 ),
-                suffixIcon: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.tune),
-                ),
-                filled: false,
-              ),
-            ),
-          ],
-        ),
+              );
+          }
+        }),
       ), // empty screen
     );
   }
