f0480de6fcf6184a84dde32bf9d3925a6e673da5
marymark-919 <mary.mark@moselaymd.com>
2025-10-27 15:41:51 +0300

    -Added reactive UI using Obx to display contact info -Added fetchContactInfo to handle fetching contact info from API

--- lib/features/profile/contact_us/contact_us_controller.dart
+++ lib/features/profile/contact_us/contact_us_controller.dart
@@ -14,42 +14,68 @@ import 'package:para_job/packages/ui_components/show_snack_bar_message.dart';
 import 'package:para_job/packages/user_manager/user_controller.dart';
 
 class ContactUsController extends GetxController {
-
- 
-  final user= Get.find<UserController>().user!;
+  final user = Get.find<UserController>().user!;
   final messageController = TextEditingController();
-   var messageError = RxnString(null);
+  var messageError = RxnString(null);
+  ContactInfoData? contactInfo;
+  var contactCallState = ApiCallState.loading.obs;
 
+  @override
+  void onInit() {
+    super.onInit();
+    fetchContactInfo();
+  }
 
+  Future<void> fetchContactInfo() async {
+    contactCallState.value = ApiCallState.loading;
 
+    try {
+      final response = await apiClient.getContactInfo();
+
+      if (response.isSuccess) {
+        log("🟢 isSuccess");
+
+        contactInfo = response.data;
+
+        contactCallState.value = ApiCallState.success;
+      } else {
+        log("🟢 isfail");
+        contactCallState.value = ApiCallState.failure;
+      }
+    } catch (e) {
+      log("🔴 ${e.toString()}");
+      contactCallState.value = ApiCallState.failure;
+    }
+  }
 
   Future<void> validateAndSubmit(BuildContext context) async {
     messageError.value = validateMessage(messageController.text);
 
-
-    if (messageError.value == null ) {
+    if (messageError.value == null) {
       await _contactUs(context);
     }
   }
-
-
 
   Future<void> _contactUs(BuildContext context) async {
     try {
       context.loaderOverlay.show();
 
-      final response = await apiClient.contactUs(ContactUsRequest(name: user.name??"", email:  user.email??"", message:messageController.text));
-
+      final response = await apiClient.contactUs(
+        ContactUsRequest(
+          name: user.name ?? "",
+          email: user.email ?? "",
+          message: messageController.text,
+        ),
+      );
 
       if (response.isSuccess) {
-      print("🔴 success");
+        print("🔴 success");
 
         //log("🟢 isSuccess");
         showSnackBarSuccess(
           "Success",
           response.details.message ?? "message sent successfully",
         );
-        
       } else {
         log("🟢 isfail");
         showSnackBarError(
@@ -59,7 +85,7 @@ class ContactUsController extends GetxController {
       }
     } catch (e) {
       print("🔴 ${e.toString()}");
-    //  log("🔴 ${e.toString()}");
+      //  log("🔴 ${e.toString()}");
       showSnackBarApiError();
     } finally {
       context.loaderOverlay.hide();
--- lib/features/profile/contact_us/contact_us_screen.dart
+++ lib/features/profile/contact_us/contact_us_screen.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:para_job/features/profile/contact_us/contact_us_controller.dart';
 import 'package:para_job/features/profile/widgets/custom_listtile_contactus.dart';
+import 'package:para_job/packages/api_client/src/enums/api_call_state_enum.dart';
 import 'package:para_job/packages/themeing/app_colors.dart';
 import 'package:para_job/packages/themeing/media_query_values.dart';
+import 'package:para_job/packages/ui_components/error_screen.dart';
 
 class ContactUsScreen extends StatelessWidget {
   ContactUsScreen({super.key});
 
-   final controller = Get.put(ContactUsController());
+  final controller = Get.put(ContactUsController());
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +52,8 @@ class ContactUsScreen extends StatelessWidget {
                       hintText: "Share your issues with us..",
                       errorText: controller.messageError.value,
                       contentPadding: EdgeInsets.symmetric(
-                        vertical: context.hPct(1),horizontal: context.wPct(2),
+                        vertical: context.hPct(1),
+                        horizontal: context.wPct(2),
                       ),
                     ),
 
@@ -61,35 +64,63 @@ class ContactUsScreen extends StatelessWidget {
                   );
                 }),
                 context.hBox(3),
+
                 Text(
                   'Contact us',
                   style: TextStyle(
                     color: AppColors.pureWhite,
                     fontSize: context.wPct(8.5),
-                    
+
                     fontWeight: FontWeight.w600,
                   ),
                 ),
-                context.hBox(2),
-                CustomListTileContactUs(title: "Email us on", subtitle: ""),
-                 context.hBox(1),
-                CustomListTileContactUs(title: "By phone", subtitle: ""),
+                context.hBox(4),
+
+                Obx(() {
+                  final contact = controller.contactInfo;
+                  switch (controller.contactCallState.value) {
+                    case ApiCallState.loading:
+                      return SizedBox(
+                        height: context.hPct(15),
+                        child: Center(child: const CircularProgressIndicator()),
+                      );
+                    case ApiCallState.success:
+                      return Column(
+                        children: [
+                          CustomListTileContactUs(
+                            title: "Email us on",
+                            subtitle: contact!.email,
+                          ),
+                          context.hBox(1),
+                          CustomListTileContactUs(
+                            title: "By phone",
+                            subtitle: contact.phoneNumber,
+                          ),
+                        ],
+                      );
+                    case ApiCallState.failure:
+                      return Center(
+                        child: ErrorScreen(
+                          onPressed: () {
+                            controller.fetchContactInfo();
+                          },
+                        ),
+                      );
+                  }
+                }),
+                context.hBox(8),
+
+                FilledButton(
+                  onPressed: () {
+                    controller.validateAndSubmit(context);
+                  },
+                  child: Text("Submit your complaint"),
+                ),
               ],
             ),
           ),
         ),
-        bottomNavigationBar: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: context.wPct(5),
-            vertical: context.hPct(12),
-          ),
-          child: FilledButton(
-            onPressed: () {
-              controller.validateAndSubmit(context);
-            },
-            child: Text("Confirm"),
-          ),
-        ),
+
       ),
     );
   }
