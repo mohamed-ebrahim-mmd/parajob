023343dc6a9adb22ba14bb116fc2e1209f04089a
marymark-919 <mary.mark@moselaymd.com>
2025-10-28 14:19:38 +0300

    Added showEditPhotoBottomSheet() to handle profile photo actions

---
+++ lib/features/profile/widgets/show_edit_photo_buttom_sheet.dart
@@ -0,0 +1,58 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:para_job/packages/themeing/app_colors.dart';
+import 'package:para_job/packages/themeing/media_query_values.dart';
+
+Future<void> showEditPhotoBottomSheet(
+  BuildContext context,
+  // SearchJobController controller,
+) async {
+  await Get.bottomSheet(
+    enterBottomSheetDuration: Duration(milliseconds: 50), // faster
+    Container(
+      padding: EdgeInsets.all(context.wPct(5)),
+      decoration: BoxDecoration(
+        color: AppColors.midnightBlue,
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(context.wPct(6)),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // button
+          context.hBox(2),
+          FilledButton(
+            onPressed: () {},
+            style: FilledButton.styleFrom(
+              backgroundColor: AppColors.whiteSoft5,
+            ),
+            child: Text("Choose photo"),
+          ),
+          context.hBox(2),
+          FilledButton(
+            onPressed: () {},
+            style: FilledButton.styleFrom(
+              backgroundColor: AppColors.whiteSoft5,
+            ),
+            child: Text("Take photo"),
+          ),
+          context.hBox(2),
+
+          FilledButton(
+            onPressed: () {},
+            style: FilledButton.styleFrom(
+              backgroundColor: AppColors.whiteSoft5,
+            ),
+            child: Text("Remove photo",style: TextStyle(color: AppColors.coralRed),),
+          ),
+          context.hBox(2),
+        ],
+      ),
+    ),
+    isScrollControlled: true,
+    backgroundColor: Colors.transparent,
+  );
+}
