2264584284dc4fa7946378f1981799b7fda33664
marymark-919 <mary.mark@moselaymd.com>
2025-11-11 13:05:24 +0200

    add ChangePassOtpScreen with OTP input and verification

--- lib/features/profile/change_pass/change_pass_otp/change_pass_otp_controller.dart
+++ lib/features/profile/change_pass/change_pass_otp/change_pass_otp_controller.dart
@@ -1,29 +1,22 @@
 //Mary Mark ||  mary.mark@moselaymd.com || Tue Nov 11 2025 12:24:42
 
-
-
-
 import 'package:flutter/cupertino.dart' show TextEditingController;
 import 'package:flutter/material.dart' show BuildContext;
 import 'package:get/get.dart';
 import 'package:loader_overlay/loader_overlay.dart';
-import 'package:para_job/features/authentication/set_new_password/set_new_password_controller.dart';
 import 'package:para_job/packages/api_client/api_client.dart';
 import 'package:para_job/packages/functional_components/validation_utils.dart';
-import 'package:para_job/packages/route_manager/controller/routes.dart';
 import 'package:para_job/packages/ui_components/show_snack_bar_message.dart';
 import 'package:para_job/packages/user_manager/user_controller.dart';
 
 class ChangePassOtpController extends GetxController {
-    final phoneNumber = Get.find<UserController>().user!.phoneNumber ?? '';
+  final phoneNumber = Get.find<UserController>().user!.phoneNumber ?? '';
 
   // TextEditingController for OTP / PIN input
   final pinController = TextEditingController();
 
   // Validation error for the PIN input
   var pinError = RxnString(null);
-
-
 
   /// ✅ Validate and proceed with verifying OTP
   Future<void> verifyOtp(BuildContext context) async {
@@ -48,7 +41,6 @@ class ChangePassOtpController extends GetxController {
 
       if (response.isSuccess ?? false) {
         showSnackBarSuccess('Success', response.details?.message ?? '');
-
       } else {
         showSnackBarError(
           'Failed',
