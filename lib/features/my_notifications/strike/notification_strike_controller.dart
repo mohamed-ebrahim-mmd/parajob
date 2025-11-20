b52991a694686345b0ff6a3d8592be3ba3a31e76
marymark-919 <mary.mark@moselaymd.com>
2025-11-20 18:02:32 +0200

    add NotificationStrikeController with strike fetching logic

---
+++ lib/features/my_notifications/strike/notification_strike_controller.dart
@@ -0,0 +1,50 @@
+//Mary Mark ||  mary.mark@moselaymd.com || Thu Nov 20 2025 17:50:49
+
+import 'dart:developer';
+
+import 'package:get/get.dart';
+
+import 'package:para_job/packages/api_client/src/enums/api_call_state_enum.dart';
+import 'package:para_job/packages/api_client/src/models/responses/strike.dart';
+import 'package:para_job/packages/api_client/src/service/api_client_instance.dart';
+import 'package:para_job/packages/user_manager/user_controller.dart';
+
+
+class NotificationStrikeController extends GetxController {
+  final userController = Get.find<UserController>();
+  var strikesCallState = ApiCallState.loading.obs;
+ List <Strike> strikesData =[];
+
+
+  @override
+  void onInit() {
+    super.onInit();
+
+  }
+ // Pages for each destination
+
+  Future<void> fetchProfileDetails() async {
+    strikesCallState.value = ApiCallState.loading;
+
+    try {
+      final response = await apiClient.fetchStrikes(
+        userController.token!,
+      );
+
+      if (response.isSuccess) {
+        log("🟢 fetchProfileDetails isSuccess");
+       strikesData  = response.data;
+
+     
+        strikesCallState.value = ApiCallState.success;
+      } else {
+        strikesCallState.value = ApiCallState.failure;
+      }
+    } catch (e) {
+      log("🔴 ${e.toString()}");
+      strikesCallState.value = ApiCallState.failure;
+    }
+  }
+
+
+}
