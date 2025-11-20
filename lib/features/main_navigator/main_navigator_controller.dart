7f2f650ade3f2c97b4886a6721bd3623faa85491
marymark-919 <mary.mark@moselaymd.com>
2025-11-20 12:20:15 +0200

    add blocked-user handling with profile fetch and conditional page initialization

--- lib/features/main_navigator/main_navigator_controller.dart
+++ lib/features/main_navigator/main_navigator_controller.dart
@@ -1,8 +1,12 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:para_job/features/home/home_screen.dart';
 import 'package:para_job/features/my_jobs/my_jobs_screen.dart';
 import 'package:para_job/features/profile/user_profile/profile_screen.dart';
+import 'package:para_job/packages/api_client/src/enums/api_call_state_enum.dart';
+import 'package:para_job/packages/api_client/src/service/api_client_instance.dart';
 import 'package:para_job/packages/ui_components/auth_required_dialog.dart';
 import 'package:para_job/packages/user_manager/user_controller.dart';
 
@@ -12,18 +16,60 @@ class MainNavigatorController extends GetxController {
   final userController = Get.find<UserController>();
   var tab = 0.obs; // Observable variable to track tab index
   var userProfilePic = Rx<String?>(null);
+  var isBlockedCallState = ApiCallState.loading.obs;
+  bool isBlocked = false;
 
   void setUserProfilePic(String? profilePic) {
     userProfilePic.value = profilePic;
   }
+  late final List<Widget> pages;
 
-  // Pages for each destination
-  late final List<Widget> pages = [
-    HomeScreen(),
-    userController.isGuest ? SizedBox() : MyJobsScreen(),
-    userController.isGuest ? SizedBox() : MyNotificationScreen(),
-    userController.isGuest ? SizedBox() : ProfileScreen(),
-  ];
+  @override
+  void onInit() {
+    super.onInit();
+    if (userController.isGuest) {
+      isBlockedCallState.value = ApiCallState.success;
+      initPages();
+
+    } else {
+      fetchProfileDetails();
+    }
+  }
+ // Pages for each destination
+  void initPages() {
+    pages = [
+      HomeScreen(),
+      userController.isGuest ? SizedBox() : MyJobsScreen(),
+      userController.isGuest ? SizedBox() : MyNotificationScreen(),
+      userController.isGuest ? SizedBox() : ProfileScreen(),
+    ];
+  }
+
+  Future<void> fetchProfileDetails() async {
+    isBlockedCallState.value = ApiCallState.loading;
+
+    try {
+      final response = await apiClient.fetchUserProfile(
+        token: userController.token!,
+      );
+
+      if (response.isSuccess) {
+        log("🟢 fetchProfileDetails isSuccess");
+        var profileData = response.data;
+
+       isBlocked = profileData.isBlocked ?? false;
+        if (!isBlocked) {
+          initPages();
+        }
+        isBlockedCallState.value = ApiCallState.success;
+      } else {
+        isBlockedCallState.value = ApiCallState.failure;
+      }
+    } catch (e) {
+      log("🔴 ${e.toString()}");
+      isBlockedCallState.value = ApiCallState.failure;
+    }
+  }
 
   Future<void> updateTab(BuildContext context, int index) async {
     switch (index) {
