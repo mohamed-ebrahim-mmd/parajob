dbf4dd9a046b12c336c381d539047b792942a7a7
mohamedebrahim7 <mohamed7ebrahim7@gmail.com>
2026-01-22 21:43:05 +0200

    refactor balance chart data logic into separate utility file

--- lib/features/profile/balance/widgets/balance_chart.dart
+++ lib/features/profile/balance/widgets/balance_chart.dart
@@ -2,6 +2,7 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:para_job/features/profile/balance/balance_controller.dart';
+import 'package:para_job/features/profile/balance/util/get_chart_data.dart';
 
 class BalanceChart extends StatelessWidget {
   BalanceChart({super.key});
@@ -10,6 +11,7 @@ class BalanceChart extends StatelessWidget {
   static const double _leftPadding = 24;
   static const double _defaultPadding = 16;
   final controller = Get.find<BalanceController>();
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -23,7 +25,10 @@ class BalanceChart extends StatelessWidget {
         ),
 
         child: LineChart(
-          controller.chartData(chart: controller.balanceData!.chart),
+          chartData(
+            chart: controller.balanceData!.chart,
+            selectedTab: controller.selectedTab,
+          ),
         ),
       ),
     );
