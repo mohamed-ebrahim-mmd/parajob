a8fd8a109a4accf91d17332c8718e00e3d8a0172
kareemToson <kareemtoson1@gmail.com>
2026-01-25 11:27:31 +0200

    refactor(profile): simplify and optimize balance chart rendering

--- lib/features/profile/balance/widgets/balance_chart.dart
+++ lib/features/profile/balance/widgets/balance_chart.dart
@@ -3,33 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:para_job/features/profile/balance/balance_controller.dart';
 import 'package:para_job/features/profile/balance/util/get_chart_data.dart';
+import 'package:para_job/packages/themeing/media_query_values.dart';
 
 class BalanceChart extends StatelessWidget {
   BalanceChart({super.key});
 
-  static const double _chartHeightFactor = 0.25;
-  static const double _leftPadding = 24;
-  static const double _defaultPadding = 16;
   final controller = Get.find<BalanceController>();
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: context.height * _chartHeightFactor,
-      child: Container(
-        padding: const EdgeInsets.fromLTRB(
-          _leftPadding,
-          _defaultPadding,
-          _defaultPadding,
-          _defaultPadding,
+    return AspectRatio(
+      aspectRatio: 4 / 3,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(
+          context.wPct(3),
+          context.hPct(2),
+          context.wPct(2.5),
+          context.hPct(2),
         ),
-
-        child: LineChart(
-          chartData(
-            chart: controller.balanceData!.chart,
-            selectedTab: controller.selectedTab,
-          ),
-        ),
+        child: LineChart(chartData(chart: controller.balanceData!.chart)),
       ),
     );
   }
