cd7901cfe98b56410ba660ab9448eafcaa1a0ec3
mohamedebrahim7 <mohamed7ebrahim7@gmail.com>
2026-04-30 15:17:36 +0300

    add Firebase Performance Monitoring for Dio

--- lib/packages/api_client/src/service/dio_singleton_instance.dart
+++ lib/packages/api_client/src/service/dio_singleton_instance.dart
@@ -1,17 +1,23 @@
 import 'package:dio/dio.dart';
+import 'package:firebase_performance_dio/firebase_performance_dio.dart';
 import 'package:get/get.dart';
 import 'package:para_job/packages/localization_manger/controller/localization_controller.dart';
 
-final dio = Dio(
-  BaseOptions(
-    connectTimeout: const Duration(minutes: 1),
-    receiveTimeout: const Duration(minutes: 1),
-    sendTimeout: const Duration(minutes: 1),
-    headers: {
-      'Accept': 'application/json',
-      'Locale': Get.find<LocalizationController>().currentLocale.languageCode,
-    },
-    receiveDataWhenStatusError: true,
-    followRedirects: true,
-  ),
-);
+final dio =
+    Dio(
+        BaseOptions(
+          connectTimeout: const Duration(minutes: 1),
+          receiveTimeout: const Duration(minutes: 1),
+          sendTimeout: const Duration(minutes: 1),
+          headers: {
+            'Accept': 'application/json',
+            'Locale':
+                Get.find<LocalizationController>().currentLocale.languageCode,
+          },
+          receiveDataWhenStatusError: true,
+          followRedirects: true,
+        ),
+      )
+      ..interceptors.add(
+        DioFirebasePerformanceInterceptor(),
+      ); // This works globally!
